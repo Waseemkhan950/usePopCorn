65d4c75debf2243bcd35fb6e7c06808e8fd60adb
Waseemkhan950 <waseemadil950@gmail.com>
2025-03-02 03:07:11 +0500

    develop star rating component which accepts props and is highly reusable...

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode, useState } from "react";
+import { createRoot } from "react-dom/client";
+import StarRating from "./components/StarRating";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// import './index.css'
+// import App from './App.jsx'
+
+function Test() {
+	const [movieRating, setMovieRating] = useState();
+	return (
+		<div>
+			<StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+			<p>This movie was rated {movieRating} stars</p>
+		</div>
+	);
+}
+
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		{/* <App /> */}
+		<Test />
+		<StarRating
+			maxRating={5}
+			message={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+		/>
+		<StarRating maxRating={5} color="green" />
+		<StarRating maxRating={5} defaultRating={3} />
+	</StrictMode>
+);
